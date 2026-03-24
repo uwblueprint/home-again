54c67c8cb223ac5cd9cfe825620f6b48cd71343d
Matthew Petersen <matthew.m.petersen@gmail.com>
2026-03-23 20:42:03 -0400

    Add intake route constants (DEV-140)

--- frontend/constants/Routes.ts
+++ frontend/constants/Routes.ts
@@ -4,3 +4,8 @@
  */
 
 export const HOME_PAGE = "/";
+
+export const INTAKE_AGENCY = "/intake/agency";
+export const INTAKE_MAIN_AGENT = "/intake/main-agent";
+export const INTAKE_OTHER_AGENTS = "/intake/other-agents";
+export const INTAKE_REVIEW = "/intake/review";

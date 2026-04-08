b9444a1fd558057fffa9eb70c4de38d03227d7f2
Nuthanan Tharmarajah <n2tharma@uwaterloo.ca>
2026-04-07 20:52:10 -0400

    more edits

---
+++ frontend/app/referral-form/dev/page.tsx
@@ -0,0 +1,9 @@
+import FurnitureForm from "@/components/referral-form/FurnitureForm"
+
+export default function ReferralFormDevPage() {
+  return (
+    <main className="mx-auto flex max-w-6xl flex-col gap-5 px-4 py-10">
+      <FurnitureForm />
+    </main>
+  )
+}
